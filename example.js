e285cd8eaf546d6e1a6b24c8963015a572e9f9ac
sebapian <sebapian95@gmail.com>
2022-07-10 23:14:51 +1200

    Uploaded javascript dom blog a& updated main css

---
+++ example.js
@@ -0,0 +1,30 @@
+// HANDSTAND ROUTINE //
+
+for (step = 0; step < 10; step++) {
+  console.log('Forward Wrist Stretch')
+}
+
+for (step = 0; step < 10; step++) {
+  console.log('Backward Wrist Stretch')
+}
+
+// ... CARRY ON WITH HANDSTAND ROUTINE ... //
+
+// variable       Index              Index            Index
+// name             0                  1                2
+dogBreeds = ['German Shephard', 'Pomeranian', 'Golden Retriever']
+
+let german = dogBreeds[0] // accessed value 'German Shephard'
+
+let germanShephard = {
+  size: 'large', // key is size, value is large
+  energyLevel: 'high', // key is energyLevel, value is high
+  adaptability: 'high', // key is adaptability, value is high
+}
+
+let germanSize = germanShephard.size // accessed value 'large'
+
+//               parameters
+function multiply(a, b) {
+  return a * b
+}
